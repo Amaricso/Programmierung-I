412129307f7d72ddc239a4b6b97967572f7a2aa7
Amaricso <an.paulfriedrich@gmail.com>
2023-10-31 12:19:06 +0100

    Add files via upload

---
+++ PR1AG/src/pr1/uebungen/HelloJava3.java
@@ -0,0 +1,13 @@
+package uebungen;
+
+public class HelloJava3 {
+	
+	public static void main(String[] args) {
+		printGreeting();
+	}
+	
+	public static void printGreeting() {
+		System.out.println("Hello Java");
+		System.out.println("3. Version");
+	}
+}
